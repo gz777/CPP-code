5312fde1ea6f3c69bf18448139834c1b85f993be
grace <grace.zhao@farmingdale.edu>
2023-03-06 10:50:48 -0500

    updated assert()

--- others/bnfParser.cpp
+++ others/bnfParser.cpp
@@ -12,29 +12,44 @@ I put some print statements in the code so that you can see the parsing trail.
 Note: The code only handles binary operations (can be nested), such as a+b*(c+d) , but not ternary or n-ary, such as a+b+c, a*b*c*d. 
       The code handles nested bracket case, such as (a+(a+(a+b))+c, or two single bracket pairs eg,(a+b)*(c+d), but not a mixer of the two, such as (a+b) * (a * (a+b)).   
 */
+/*
+The code is a prototype that it computes the value to verify the correctness of the grammer recursive flow.
+Ideally, for each nontermal function, there should be SUCCESS and FAILED cases to simulate a parser.
+ if()...
+    return SUCCESS;
+ else
+    return FAILED;
+*/
 
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
 using std::string;
+using std::cout;
+using std::endl;
+
 
 int expr(string);
 
 int factor  (string s) {
-     std::cout <<  "factor " << s << std::endl;
+    cout <<  "factor " << s << endl;
     if (s[0] == '(') {
         string ss = s.substr(1, s.size()-2);//last ele -1, ')' -1
         return expr(ss);
     } else { 
         int len = s.length(), digit = 0;
-        for(int i=0;i<len;i++)
-            digit += (s[len-i-1]-48) * pow(10,i);//ascii code
-        return digit;
+        for(int i=0;i<len;i++) {
+            int v = s[len-i-1]-48;//ascii code
+            assert(v >= 0 && v <= 9); // not a number. FAILED
+            digit += (v * pow(10,i));
+        }
+        return digit; //SUCCESS
     }
 }
 
 int term (string s) {
-    std::cout <<  "term " << s << std::endl;
+    cout <<  "term " << s << std::endl;
     size_t found = s.find('*');
     if (found!=string::npos) {
         string x = s.substr(0,found), y = s.substr(found+1);
@@ -46,7 +61,7 @@ int term (string s) {
 
 int expr(string s) {
     size_t foundp = s.find('+'), foundm = s.find('*'), foundb = s.find('(');
-    std::cout <<  "expr " << s << std::endl;
+    cout <<  "expr " << s << endl;
     if (foundb != string::npos) {
         int last_close_bracket = (s.rfind('(') > s.find(')')) ?s.find(')')  : s.rfind(')');//nested (()) or single set of ()
             
@@ -66,20 +81,26 @@ int expr(string s) {
         int ix = term(x), iy = expr(y);
         return ix + iy;
     } 
-    
-        
-    return term(s);
+    else
+        return term(s);
 
 }
 
 int main() {
 
-    //std::cout << factor("234");
-    //std::cout << term("2*44");
-    std::cout << expr("2*3+3") << "\n\n";
-    std::cout << expr("(2*3+3)*(2+3)") << "\n\n";
-    std::cout << expr("2*(3+(4+(1+2)))") << "\n\n";
+    //success cases
+    //cout << factor("234");
+    //cout << term("2*44");
+    cout << expr("2*3+3") << "\n\n"
+         << expr("(2*3+3)*(2+3)") << "\n\n"
+         << expr("2*(3+(4+(1+2)))") << "\n\n"
+         << expr("2*3*(4+1+2)") << "\n\n";
+    
+    //failed case
+    cout << expr("2*3^(4+1+2)") << "\n\n"; // the grammar doesn't have knowledge how to handle '/'. FAILED case.
 
 
     return 0;
 }
+
+
